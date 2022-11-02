f7b11af1e62c5c8cdd61090a72803c8b32dbb401
srdtrk <srdtrk@hotmail.com>
2022-11-02 16:25:52 +0300

    fix queries, add revoke query permits execute msg, fix clippy warnings

--- src/msg.rs
+++ src/msg.rs
@@ -1,21 +1,31 @@
-use cosmwasm_std::Addr;
 use schemars::JsonSchema;
 use secret_toolkit::permit::Permit;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 pub struct InstantiateMsg {
     pub entropy: String,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    CreateViewingKey { entropy: String },
-    SetViewingKey { key: String },
+    CreateViewingKey {
+        entropy: String,
+    },
+    SetViewingKey {
+        key: String,
+    },
+    /// disallow the use of a permit
+    RevokePermit {
+        /// name of the permit that is no longer valid
+        permit_name: String,
+        /// optional message length padding
+        padding: Option<String>,
+    },
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     QueryEx {},
@@ -39,7 +49,7 @@ impl QueryMsg {
 }
 
 /// queries using permits instead of viewing keys
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryWithPermit {
     Permissioned {},
--- src/state.rs
+++ src/state.rs
@@ -9,8 +9,11 @@ use cosmwasm_std::Addr;
 pub static CONFIG_KEY: Item<Config> = Item::new(b"config");
 /// Revoked permits prefix key
 pub const PREFIX_REVOKED_PERMITS: &str = "revoked_permits";
+/// pad handle responses and log attributes to blocks of 256 bytes to prevent leaking info based on
+/// response size
+pub const BLOCK_SIZE: usize = 256;
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 pub struct Config {
     pub owner: Addr,
     pub contract_address: Addr,
